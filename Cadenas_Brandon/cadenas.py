f7c4e59964ed8230b326be9b5e1d79d9f0ae60c5
Brandon <bchampagne50@programmecdm.com>
2025-12-04 13:56:37 -0500

    Cadenas Completed

--- Cadenas_Brandon/cadenas.py
+++ Cadenas_Brandon/cadenas.py
@@ -5,140 +5,163 @@ import time
 from RPLCD.i2c import CharLCD
 import RPi.GPIO as GPIO
 import random
+from MQTTClient import MQTTWrapper
+
+# ----- Component's Pin ----- #
+
+up_btn_PIN = 22
+down_btn_PIN = 20
+confirm_btn_PIN = 12
+left_btn_PIN = 19
+right_btn_PIN = 26
+buzzer_PIN = 16
+green_LED_PIN = 23
+red_LED_PIN = 25
+
+
+# ---- Component's GPIO Setup ----- #
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 lcd = CharLCD('PCF8574', 0x27)
+up_btn = GPIO.setup(up_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+down_btn = GPIO.setup(down_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+confirm_btn = GPIO.setup(confirm_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+left_btn = GPIO.setup(left_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+right_btn = GPIO.setup(right_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(buzzer_PIN, GPIO.OUT)
+buzzer = GPIO.PWM(buzzer_PIN,1000)
+greenLED = GPIO.setup(green_LED_PIN, GPIO.OUT)
+redLED = GPIO.setup(red_LED_PIN, GPIO.OUT)
 
-UP_btn_PIN = 25
-DOWN_btn_PIN = 24
-CONFIRM_btn_PIN = 18
-LEFT_btn_PIN = 12
-RIGHT_btn_PIN = 20
-Buzzer_PIN = 16
 
-UP_btn = GPIO.setup(UP_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-DOWN_btn = GPIO.setup(DOWN_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-CONFIRM_btn = GPIO.setup(CONFIRM_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-LEFT_btn = GPIO.setup(LEFT_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-RIGHT_btn = GPIO.setup(RIGHT_btn_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(Buzzer_PIN, GPIO.OUT)
-Buzzer = GPIO.PWM(Buzzer_PIN,1000)
+# ----- Global Variables ----- #
 
-try:
-    unlockCode = [0,0,0,0]
-    passcode = [0,0,0,0]
-    lcd.cursor_pos = (0, 0)
-    currentDigit = 0
+gameUnlocked = False
+unlockCode = [0,0,0,0]
+passcode = [0,0,0,0]
+lcd.cursor_pos = (0, 0)
+currentDigit = 0
+mutex = threading.Lock()
 
-    mutex = threading.Lock()
+# ----- Main Code ----- #
+def GenerateUnlockCode():
+    global unlockCode
+    for i in range(4):
+        unlockCode[i] = random.randint(0, 9)
 
-    def GenerateUnlockCode():
-        global unlockCode
-        for i in range(4):
-            unlockCode[i] = random.randint(0, 9)
+def showPasscode(passcode) :
+    display = "#    "
+    for digit in passcode:
+        display += str(digit) + " "
+    return display
 
-    def showPasscode(passcode) :
-        display = "#    "
-        for digit in passcode:
-            display += str(digit) + " "
-        return display
+def ChangeCurrentDigit(value):
+    global currentDigit
+    if value == 'left':
+        if currentDigit == 0:
+            currentDigit = 3
+        else:
+            currentDigit = currentDigit - 1
+    elif value == 'right':
+        if currentDigit == 3:
+            currentDigit = 0
+        else:
+            currentDigit = currentDigit + 1
+    mutex.acquire()
+    lcd.clear()
+    lcd.write_string(showPasscode(passcode))
+    mutex.release()
 
-    def ChangeCurrentDigit(value):
-        global currentDigit
-        if value == 'left':
-            if currentDigit == 0:
-                currentDigit = 3
+def ChangeCurrentValue(value):
+    global passcode
+    if value == 'up':
+        if passcode[currentDigit] == 9:
+            passcode[currentDigit] = 0
+        else:
+            passcode[currentDigit] = passcode[currentDigit] + 1
+    elif value == 'down':
+        if passcode[currentDigit] == 0:
+            passcode[currentDigit] = 9
+        else:
+            passcode[currentDigit] = passcode[currentDigit] - 1
+    mutex.acquire()
+    lcd.clear()
+    lcd.write_string(showPasscode(passcode))
+    mutex.release()
+
+def EndGame():
+    global gameUnlocked
+    gameUnlocked = True
+    GPIO.output(green_LED_PIN, GPIO.HIGH)
+    GPIO.output(red_LED_PIN, GPIO.LOW)
+
+def numberValueChange():
+    while gameUnlocked == False:
+        etat_up_button = GPIO.input(up_btn_PIN)
+        etat_down_button = GPIO.input(down_btn_PIN)
+        etat_confirm_button = GPIO.input(confirm_btn_PIN)
+        etat_left_button = GPIO.input(left_btn_PIN)
+        etat_right_button = GPIO.input(right_btn_PIN)
+        if etat_left_button == GPIO.HIGH:
+            ChangeCurrentDigit('left')
+            sleep(0.3)
+        elif etat_right_button == GPIO.HIGH:
+            ChangeCurrentDigit('right')
+            sleep(0.3)
+        elif etat_up_button == GPIO.HIGH:
+            ChangeCurrentValue('up')
+            sleep(0.3)
+        elif etat_down_button == GPIO.HIGH:
+            ChangeCurrentValue('down')
+            sleep(0.3)
+        elif etat_confirm_button == GPIO.HIGH:
+            if passcode == unlockCode:
+                EndGame()
+                mutex.acquire()
+                lcd.clear()
+                lcd.cursor_pos = (0, 3)
+                lcd.write_string("Unlocked!")
+                buzzer.start(100)
+                time.sleep(1)
+                buzzer.stop()
+                mutex.release()
+                break
             else:
-                currentDigit = currentDigit - 1
-        elif value == 'right':
-            if currentDigit == 3:
-                currentDigit = 0
-            else:
-                currentDigit = currentDigit + 1
-        mutex.acquire()
-        lcd.clear()
-        lcd.write_string(showPasscode(passcode))
-        mutex.release()
+                mutex.acquire()
+                lcd.cursor_pos = (1, 2)
+                lcd.write_string("Wrong Code!")
+                mutex.release()
+                sleep(2)
+                mutex.acquire()
+                lcd.cursor_pos = (1, 0)
+                lcd.write_string(" " * 16)
+                mutex.release()
+            sleep(0.3)
 
-    def ChangeCurrentValue(value):
-        global passcode
-        if value == 'up':
-            if passcode[currentDigit] == 9:
-                passcode[currentDigit] = 0
-            else:
-                passcode[currentDigit] = passcode[currentDigit] + 1
-        elif value == 'down':
-            if passcode[currentDigit] == 0:
-                passcode[currentDigit] = 9
-            else:
-                passcode[currentDigit] = passcode[currentDigit] - 1
-        mutex.acquire()
-        lcd.clear()
-        lcd.write_string(showPasscode(passcode))
-        mutex.release()
-
-
-    def numberValueChange():
-        while True:
-            etat_UP_button = GPIO.input(UP_btn_PIN)
-            etat_Down_button = GPIO.input(DOWN_btn_PIN)
-            etat_Confirm_button = GPIO.input(CONFIRM_btn_PIN)
-            etat_Left_button = GPIO.input(LEFT_btn_PIN)
-            etat_Right_button = GPIO.input(RIGHT_btn_PIN)
-            if etat_Left_button == GPIO.HIGH:
-                ChangeCurrentDigit('left')
-                sleep(0.3)
-            elif etat_Right_button == GPIO.HIGH:
-                ChangeCurrentDigit('right')
-                sleep(0.3)
-            elif etat_UP_button == GPIO.HIGH:
-                ChangeCurrentValue('up')
-                sleep(0.3)
-            elif etat_Down_button == GPIO.HIGH:
-                ChangeCurrentValue('down')
-                sleep(0.3)
-            elif CONFIRM_btn == GPIO.HIGH:
-                if passcode == unlockCode:
-                    mutex.acquire()
-                    lcd.clear()
-                    lcd.write_string("Unlocked!")
-                    mutex.release()
-                    Buzzer.start(100)
-                    time.sleep(1)
-                    Buzzer.stop()
-                    sleep(2)
-                    GenerateUnlockCode()
-                    mutex.acquire()
-
-                else:
-                    mutex.acquire()
-                    lcd.cursor_pos = (1, 0)
-                    lcd.write_string("Wrong Code!")
-                    mutex.release()
-                    sleep(2)
-                    mutex.acquire()
-                    lcd.cursor_pos = (1, 0)
-                    lcd.write_string(" " * 16)
-                    mutex.release()
-                sleep(0.3)
-
-    def blinkingCurrentValue():
-        global currentDigit
-        while True:
+def blinkingCurrentValue():
+    global currentDigit
+    try:
+        while gameUnlocked == False:
+            print("blinkingCurrentValue", currentDigit)
             mutex.acquire()
             lcd.cursor_pos = (0, 5 + currentDigit * 2)
             lcd.write_string(" ")
             mutex.release()
             sleep(0.5)
+            if gameUnlocked:
+                break
             mutex.acquire()
             lcd.cursor_pos = (0, 5 + currentDigit * 2)
             lcd.write_string(str(passcode[currentDigit]))
             mutex.release()
             sleep(0.5)
+    except Exception as e:
+        print("Error in blinkingCurrentValue:", e)
 
 
-    def buzzerMorseCode():
+def buzzerMorseCode():
+    try:
         morse_code_dict = {
             '0': '-----',
             '1': '.----',
@@ -153,50 +176,79 @@ try:
         }
         print(unlockCode)
         code = ''.join(map(str, unlockCode))
-        while True:
-            mutex.acquire()
-            lcd.cursor_pos = (1, 5)
-            lcd.write_string("Listen!")
-            mutex.release()
+        while gameUnlocked == False:
             for digit in code:
+                if gameUnlocked:
+                    break
+                mutex.acquire()
+                lcd.cursor_pos = (1, 5)
+                lcd.write_string("Listen!")
+                mutex.release()
                 morse_code = morse_code_dict[digit]
                 for symbol in morse_code:
+                    if gameUnlocked:
+                        break
                     if symbol == '.':
-                        Buzzer.start(50)
+                        buzzer.start(50)
                         time.sleep(0.2)
-                        Buzzer.stop()
+                        buzzer.stop()
                         time.sleep(0.2)
                     elif symbol == '-':
-                        Buzzer.start(50)
+                        buzzer.start(50)
                         time.sleep(0.8)
-                        GPIO.output(Buzzer_PIN, GPIO.LOW)
-                        Buzzer.stop()
+                        GPIO.output(buzzer_PIN, GPIO.LOW)
+                        buzzer.stop()
                         time.sleep(0.2)
                 time.sleep(1.0)
+            print("buzzerMorseCode 1")
             mutex.acquire()
             lcd.cursor_pos = (1, 0)
             lcd.write_string(" " * 16)
             mutex.release()
             time.sleep(5)
+            print("buzzerMorseCode 2")
+        print("buzzerMorseCode 3")
+    except Exception as e:
+        print("Error in buzzerMorseCode:", e)
 
-    GenerateUnlockCode()
-    lcd.clear()
-    lcd.write_string(showPasscode(passcode))
+def StartGame():
+    try:
+        global gameUnlocked
+        gameUnlocked = False
+        GenerateUnlockCode()
+        GPIO.output(green_LED_PIN, GPIO.LOW)
+        GPIO.output(red_LED_PIN, GPIO.HIGH)
+        lcd.clear()
+        lcd.write_string(showPasscode(passcode))
 
-    thread_numberValueChange = threading.Thread(target=numberValueChange)
-    thread_blinkingCurrentValue = threading.Thread(target=blinkingCurrentValue)
-    thread_buzzerMorseCode = threading.Thread(target=buzzerMorseCode)
+        thread_numberValueChange = threading.Thread(target=numberValueChange)
+        thread_blinkingCurrentValue = threading.Thread(target=blinkingCurrentValue)
+        thread_buzzerMorseCode = threading.Thread(target=buzzerMorseCode)
+        #numberValueChange()
 
-    thread_numberValueChange.start()
-    thread_blinkingCurrentValue.start()
-    thread_buzzerMorseCode.start()
-    thread_numberValueChange.join()
-    thread_blinkingCurrentValue.join()
-    thread_buzzerMorseCode.join()
+        thread_numberValueChange.start()
+        thread_blinkingCurrentValue.start()
+        thread_buzzerMorseCode.start()
+        thread_numberValueChange.join()
+        print("JOIN 1")
+        thread_blinkingCurrentValue.join()
+        print("JOIN 2")
+        thread_buzzerMorseCode.join()
 
-except KeyboardInterrupt:
-    lcd.clear()
-    GPIO.cleanup()
-    thread_blinkingCurrentValue.running = False
-    thread_numberValueChange.running = False
-    thread_buzzerMorseCode.running = False
+    except KeyboardInterrupt:
+        
+        thread_blinkingCurrentValue.running = False
+        thread_numberValueChange.running = False
+        thread_buzzerMorseCode.running = False
+        GPIO.cleanup()
+        lcd.clear()
+        print("Program stopped by User")
+    except Exception as e:
+        print("An error occurred:", e)
+
+while True:
+    #mqttWapper = MQTTWrapper()
+    #mqttWapper.start()
+    #mqttWapper.wait_for_start()
+    StartGame()
+    #mqttWapper.publish_state("SUCCESS")
